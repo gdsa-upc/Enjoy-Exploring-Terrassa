adb2474146af439c83d214a620cebe4c6302a524
Bernat Vilella Fite <brianbvf@gmail.com>
2016-11-29 23:19:37 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,22 +1,13 @@
 # -*- coding: utf-8 -*-
-# Proyecto 101.2
-
-import cv2
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os #carreguem la llibreria os per tal d'obtenir la ruta absoluta de la carpeta del projecte
 
-img = cv2.imread('C:\Users\Albert\Documents\UNI\Q-5\GDSA\Projecte\testimage.jpg',0)
-
-# Pasamos a nivel de gris
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-# Iniciamos el detector SIFT
-sift = cv2.SIFT()
-
-# Detectamos puntos de interés, los computamos y los ubicamos
-kp, des = sift.detectAndCompute(gray,None)
-
-# Dibuja los puntos en una imagen de salida llamada 'sift_keypoints.jpg'
-img = cv2.drawKeypoints(gray,kp)
-
-# Creamos la imagen resultante
-cv2.imwrite('sift_keypoints.jpg',img)
+ruta = os.path.dirname(os.path.abspath(__file__)) #obtenim la ruta absoluta de la carpeta del projecte
+img = cv2.imread(ruta+'/desconegut_257.jpg',1) #obrim la imatge que hi es a la carpeta images
+surf = cv2.SURF(4000) #posem el llindar hessian a 4000
+kp, des = surf.detectAndCompute(img, None) 
+img2 = cv2.drawKeypoints(img, kp, None, (255,0,0), 4) #dibuixem els punts d'interes i ho guardem a la variable img2
+plt.imshow(img2),plt.show() #mostrem la imatge amb els punts d'interes dibuixats
+print len(kp) #mostra el nom de punts d'interés que hi han a la imatge
