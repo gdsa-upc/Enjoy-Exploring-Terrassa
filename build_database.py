a484add6b4c09a75ee17f8cbf138c04b56847d6b
Bernat Vilella Fite <brianbvf@gmail.com>
2016-12-14 01:41:47 +0100

    Update build_database.py

--- build_database.py
+++ build_database.py
@@ -1,11 +1,29 @@
-# -*- coding: utf-8 -*-
-import os # llibreria os per poder accedir al directori de treball
-def build_database(dir_entrada,val_or_train,dir_sortida):
-    images = os.listdir(dir_entrada) #Llegim el nom dels fitxers que hi ha en el directori que volem (el d'entrada)
-    outfile = open(dir_sortida+'/outfile_'+val_or_train+'.txt','w') # Creem un fitxer per guardar les id's  
-    for file in images:
-        outfile.write(file[0:-4]+"\n") #cada linea del nou fitxer serà el nom de les imatge que hi ha en el directori menys l'extensio .jpg
+import os
+from params import get_params
 
-ruta = os.path.dirname(os.path.abspath(__file__)) #ruta absoluta del projecte   
-build_database(ruta+'/TerrassaBuildings900/train/images',"train",ruta+'/files'); #creacio train database
-build_database(ruta+'/TerrassaBuildings900/val/images',"val",ruta+'/files'); #creacio val database
+def build_database(params):
+
+    # List images
+    image_names = os.listdir(os.path.join(params['root'],params['database'],params['split'],'images'))
+
+    # File to be saved
+    file = open(os.path.join(params['root'],params['root_save'],params['image_lists'],params['split'] + '.txt'),'w')
+
+    # Save image list to disk
+    for imname in image_names:
+        file.write(imname + "\n")
+    file.close()
+
+
+if __name__=="__main__":
+
+    params = get_params()
+    
+    # Build image list for validation set
+    build_database(params)
+    
+    # Switch to training set
+    params['split'] = 'train'
+    
+    # Build image list for training set
+    build_database(params)
